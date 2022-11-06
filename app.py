f76ab469a42499de7f6373d219df9d5b0443ce6a
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-11-05 21:20:50 -0600

    reestructuracion del formato json de la API

--- app.py
+++ app.py
@@ -11,16 +11,26 @@ app.config['SECRET_KEY'] = "jcp_helper233445"
 db=SQLAlchemy(app)
 
 class Prueba(db.Model):
-    __tablename__="Preguntas"
-    idPregunta=db.Column(db.Integer,primary_key=True,unique=True,nullable=False)
-    enunciado=db.Column(db.String(500),nullable=False)
+    __tablename__="Cuestionarios"
+    # idPregunta=db.Column(db.Integer,primary_key=True,unique=True,nullable=False)
+    # enunciado=db.Column(db.String(500),nullable=False)
+    # autor=db.Column(db.String(200))
+    # fecha=db.Column(db.String(20),nullable=False)#2022-06-18
+    # tema=db.Column(db.String(250))
+    # tipo_pregunta=db.Column(db.String(100),nullable=False)
+    # lenguaje=db.Column(db.String(50),nullable=False)
+    # opciones=db.Column(db.String(800))
+    # respuesta=db.Column(db.String(800),nullable=False)
+
+    idCuestionario=db.Column(db.Integer,primary_key=True,unique=True,nullable=False)
     autor=db.Column(db.String(200))
+    temas=db.Column(db.String(100),nullable=False)
     fecha=db.Column(db.String(20),nullable=False)#2022-06-18
-    tema=db.Column(db.String(250))
-    tipo_pregunta=db.Column(db.String(100),nullable=False)
+    tipo=db.Column(db.String(50),nullable=False)
     lenguaje=db.Column(db.String(50),nullable=False)
-    opciones=db.Column(db.String(800))
-    respuesta=db.Column(db.String(800),nullable=False)
+    preguntas=db.Column(db.String(20000),nullable=False)
+
+
 
     def to_dict(self):
         dictionary = {}
@@ -28,73 +38,48 @@ class Prueba(db.Model):
             dictionary[column.name] = getattr(self, column.name)
         return dictionary
 
-#db.create_all()
+db.create_all()
 
 
-@app.route("/preguntas", methods=["GET"])
-def preguntas():
+@app.route("/cuestionarios", methods=["GET"])
+def cuestionarios():
     query_lenguaje=request.args.get("lenguaje")
 
     #sí se ingresa un lenguaje se realiza la busqueda en base a ese lenguaje
     if query_lenguaje:
-        preguntas=db.session.query(Prueba).filter_by(lenguaje=query_lenguaje).all()
-        if preguntas:
-            return jsonify(preguntas=[pregunta.to_dict() for pregunta in preguntas])
+        cuestionarios=db.session.query(Prueba).filter_by(lenguaje=query_lenguaje).all()
+        if cuestionarios:
+            return jsonify(cuestionarios=[cuestionario.to_dict() for cuestionario in cuestionarios])
         else:
             return jsonify(response={"Not found": "No hay resultados para ese lenguaje."})
     else:
-        #sí no, se muestran todos los datos de los preguntas
-        preguntas = db.session.query(Prueba).all()
-        if preguntas:
-            return jsonify(preguntas=[pregunta.to_dict() for pregunta in preguntas])
+        #sí no, se muestran todos los datos de los cuestionarios
+        cuestionarios = db.session.query(Prueba).all()
+        if cuestionarios:
+            return jsonify(cuestionarios=[cuestionario.to_dict() for cuestionario in cuestionarios])
         else:
-            return jsonify(response={"Not found": "Aún no hay preguntas registradas."})
+            return jsonify(response={"Not found": "Aún no hay cuestionarios registradas."})
 
 
-# @app.route("/colonias", methods=["GET"])
-# def colonias():
-#     cp_col=request.args.get("codigop")
-#     query_colonia=request.args.get("colonia")
-
-#     if cp_col:
-#         colonias=db.session.query(Colonia).filter_by(codigop=cp_col).all()
-#         if colonias:
-#             return jsonify(colonias=[colonia.to_dict() for colonia in colonias])
-#         else:
-#             return jsonify(response={"Not found": "No hay resultados para ese código postal."})
-#     elif query_colonia:
-#         colonias=db.session.query(Colonia).filter_by(colonia=query_colonia).all()
-#         if colonias:
-#             return jsonify(colonias=[colonia.to_dict() for colonia in colonias])
-#         else:
-#             return jsonify(response={"Not found": "No hay resultados para esa colonia."})
-#     else:
-#         colonias = db.session.query(Colonia).all()
-#         if colonias:
-#             return jsonify(colonias=[colonia.to_dict() for colonia in colonias])
-#         else:
-#             return jsonify(response={"Not found": "Aún no hay colonias registradas."})
 
 
-@app.route("/nueva_pregunta", methods=["POST"])
-def pregunta_post():
+@app.route("/nuevo_cuestionario", methods=["POST"])
+def cuestionario_post():
     try:
         now=datetime.datetime.now()
-        nueva_pregunta = Prueba(
-            enunciado=request.args.get("enunciado"),
+        nuevo_cuestionario = Prueba(
             autor=request.args.get("autor"),
+            temas= request.args.get("temas"),
             fecha=now.date(),
-            tema= request.args.get("tema"),
-            tipo_pregunta= request.args.get("tipo_pregunta"),
+            tipo= request.args.get("tipo"),
             lenguaje= request.args.get("lenguaje"),
-            opciones= request.args.get("opciones"),
-            respuesta=request.args.get("respuesta")
+            preguntas=request.args.get("preguntas")
         )
-        db.session.add(nueva_pregunta)
+        db.session.add(nuevo_cuestionario)
         db.session.commit()
-        return jsonify(response={"success": "Pregunta agregada con éxito."})
+        return jsonify(response={"success": "Cuestionario agregado con éxito."})
     except:
-        return jsonify(response={"error": "Ha ocurrido un error al agregar la pregunta."})
+        return jsonify(response={"error": "Ha ocurrido un error al agregar el cuesntionario."})
 
 @app.route('/',methods=['GET'])
 def home():
